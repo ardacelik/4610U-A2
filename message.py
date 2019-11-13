a38ffd9952c891074b0bd98adb7c2355b00a01ac
Arda Celik <arda.celik@uoit.net>
2019-11-12 20:45:23 -0500

    Commented and finalized

--- message.py
+++ message.py
@@ -2,12 +2,14 @@ import EV3BT
 import time
 import serial
 
+# Open a serial port
 EV3 = serial.Serial(port='/dev/rfcomm0', baudrate=9600, timeout=None)
 print (EV3.name)
 print (EV3.is_open)
 counter = 1
 try:
     while (1):
+        # Send message
         if counter == 1:
             send = EV3BT.encodeMessage(EV3BT.MessageType.Numeric,'hello',1)
             counter += 1
@@ -16,9 +18,11 @@ try:
             counter -= 1
         EV3.write(send)
         n = 0
+        # Get message
         while (n==0):
             n=EV3.inWaiting()
         s = EV3.read(n)
+        # Decode message and print
         mail,values,s = EV3BT.decodeMessage(s,EV3BT.MessageType.Text)
         print (mail,values)
         time.sleep(1)
